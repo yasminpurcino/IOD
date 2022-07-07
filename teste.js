cc2f0aea089b2b8cdb436df26127d796a2e87ade
yasminpurcino <108705820+yasminpurcino@users.noreply.github.com>
2022-07-07 16:26:36 +1200

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,73 @@
+
+
+//var name="Yasmin"
+//console.log(name)
+
+//var name="Ana"
+//console.log(name)
+
+//var item_number =10
+//console.log(item_number)
+
+//let school_name="IOD"
+//console.log(school_name)
+
+//const city_name="Auckland"
+//console.log(city_name)
+
+
+
+//let city_name="Sidney"
+//console.log(city_name)
+
+
+//var item_number =10
+//console.log(item_number)
+
+//let school_name="IOD"
+//console.log(school_name)
+
+//const city_name="Auckland"
+//console.log(city_name)
+
+
+//let employees=["Yasmin","Ana", "Carol", "Bia", "John"]
+//console.log(employees)
+//console.log(employees.length)
+//console.log(employees[3])
+//console.log(employees[6])
+
+//employees [3]="Joey"
+//console.log(employees)
+
+//let cars=["a","b", "c", "d", "e"]
+//console.log(Array.isArray(cars))
+//console.log(cars.indexOf("b"))
+
+//console.log(cars.pop())
+
+//console.log(cars)
+//cars.push("e")
+
+//console.log(cars.shift())
+//console.log(cars)
+
+//console.log(cars.unshift("y"))
+
+//console.log(cars)
+
+let car_obj = {
+"model":"a",
+"year":"2020",
+"brand":"c",
+"color":"black"
+}
+
+console.log(car_obj)
+
+console.log(car_obj.year)
+console.log(car_obj["year"])
+
+car_obj.model="d"
+
+console.log(car_obj)
