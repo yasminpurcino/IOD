e48bbdd2384d8005e0e9ad196f236e8461250755
Jo Batkin <jo.batkin@institutedata.com>
2022-08-03 12:44:20 +1000

    reviewed

--- 3Week/script2.js
+++ 3Week/script2.js
@@ -17,16 +17,16 @@ function addCard() {
 
     }
 
-    const artist={
-name:"Van Gogh",
-portfolio:[
-{title:"portrait",
+//     const artist={
+// name:"Van Gogh",
+// portfolio:[
+// {title:"portrait",
 
-url:"https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image"},
+// url:"https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image"},
 
-{title:"sky",
+// {title:"sky",
 
-url:"https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg"},
+// url:"https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg"},
 
-]
-}
+// ]
+// }
