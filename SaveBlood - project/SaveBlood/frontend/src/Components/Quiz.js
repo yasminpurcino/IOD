77acc168e1860f136e115ac6e96668c611f97e78
Yasmin Purcino <yayapurcino@gmail.com>
2022-09-21 18:34:55 +1200

    quiz form is working now

--- SaveBlood - project/SaveBlood/frontend/src/Components/Quiz.js
+++ SaveBlood - project/SaveBlood/frontend/src/Components/Quiz.js
@@ -3,150 +3,100 @@ import Logo from './Logo.png'
 
 
 function Quiz() {
-  return (
-  
-    <div>
-     <div className="imgLogoQuiz">
-      <img src={Logo} alt="logo" />
-     </div>
+    return (
+
+        <div>
+            <div className="imgLogoQuiz">
+                <img src={Logo}
+                    alt="logo"/>
+            </div>
+            <div className="titleQuiz">Am I eligible to become a Donor ?
+            </div>
+
+            <div className="textQuiz">
+                In order to donate blood, plasma or platelets, you must pass a list of criteria to ensure that your donation is safe
+            </div>
+            <div>
+                <form className='formQuiz'>
+                    <h3 className='textForm'>your age?
+                    </h3>
+                    <label>
+                        under 18
+                        <input className='radioForm' name="age" type="radio"/>
+                    </label>
+                    <label>
+                        above 18
+                        <input className='radioForm' name="age" type="radio"/>
+                    </label>
+
+                    <h3 className='textForm'>your weight?
+                    </h3>
+                    <label>
+                        under 51kg
+                        <input className='radioForm' name="weight" type="radio"/>
+                    </label>
+                    <label>
+                        over 51kg
+                        <input className='radioForm' name="weight" type="radio"/>
+                    </label>
 
 
-      <div className="titleQuiz">Am I eligible to become a Donor ?
-      </div>
-    
-      <div className="textQuiz">
-       In order to donate blood, plasma or platelets, you must pass a list of criteria to ensure that your donation is safe
-      </div>
-    
-      <div>
-        <fieldset className='formQuiz'>
-          <legend className='textForm'>your age? </legend>
-          <input
-            type="radio"
-            id="<18"
-            name="age"
-            value="<18"
-                        
-          />
-          <label className='label' htmlFor="<18"> under 18
-          </label>
-                  
-          <br />
-          <input 
-          type="radio"
-          id=">18"
-          name="age"
-          value="no"
-                    
-          />
-          <label className='label' htmlFor="no">over 18</label>
-          
-          <br />
-          <br></br>
+                    <h3 className='textForm'>your height?
+                    </h3>
+                    <label>
+                        under 151cm
+                        <input className='radioForm' name="height" type="radio"/>
+                    </label>
+                    <label>
+                        over 151cm
+                        <input className='radioForm' name="height" type="radio"/>
+                    </label>
 
-          <legend className='textForm'>your weight? </legend>
-          <input 
-            type="radio"
-            id="<51"
-            name="weight"
-            value="<51"
-                    
-          />
-          <label className='label' htmlFor="<51"> under 51kg</label>
-              
-          <br />
-          <input 
-            type="radio"
-            id=">51"
-            name="weight"
-            value="no"
-                  
-          />
-          <label className='label' htmlFor="no">over 51kg</label>
-          
-          <br />
-          <br></br>
 
-          <legend className='textForm'>your height? </legend>
-          <input 
-            type="radio"
-            id="<151cm"
-            name="height"
-            value="yes"
-                        
-          />
-          <label className='label' htmlFor="<151cm">under 151cm</label>
-                  
-          <br />
-          <input 
-            type="radio"
-            id=">151cm"
-            name="antibiotics"
-            value="no"
-                    
-          />
-          <label className='label' htmlFor=">151cm"> over 151cm</label>
-          
-          <br />
-          <br></br>
+                    <h3 className='textForm'>have you had a tattoo or piercing in the last 3 months?
+                    </h3>
+                    <label>
+                        yes
+                        <input className='radioForm' name="tattoo" type="radio"/>
+                    </label>
+                    <label>
+                        no
+                        <input className='radioForm' name="tattoo" type="radio"/>
+                    </label>
 
-          <legend className='textForm'>have you had a tattoo or piercing in the last 3 months? </legend>
-          <input 
-            type="radio"
-            id="yes"
-            name="tattoo"
-            value="yes"
-                
-          />
-          <label className='label' htmlFor="yes">yes</label>
-          
-          <br />
-          <input 
-            type="radio"
-            id="no"
-            name="tattoo"
-            value="no"
-            
-          />
-          <label className='label' htmlFor="no">no</label>
-      
-          <br />
+                    <h3 className='textForm'>have you been pregnant or given in the last 9 months?
+                    </h3>
+                    <label>
+                        yes
+                        <input className='radioForm' name="pregnant" type="radio"/>
+                    </label>
+                    <label>
+                        no
+                        <input className='radioForm' name="pregnant" type="radio"/>
+                    </label>
 
-          <br></br>
+                    <h3 className='textForm'>have you taken antibiotics in the last seven days?
+                    </h3>
+                    <label>
+                        yes
+                        <input className='radioForm' name="antibiotics" type="radio"/>
+                    </label>
+                    <label>
+                        no
+                        <input className='radioForm' name="antibiotics" type="radio"/>
+                    </label>
 
-          <legend className='textForm'>have you been pregnant or given in the last 9 months?</legend>
-          <input 
-            type="radio"
-            id="yes"
-            name="pregnant"
-            value="yes"
-        
-          />
-          <label className='label' htmlFor="yes">yes</label>
+                </form>
+            </div>
 
-          <br />
-          <input 
-            type="radio"
-            id="no"
-            name="pregnant"
-            value="no"
+            <button type="submit" className="btnQuiz">
+                Submit
+            </button>
 
-          />
-          <label className='label' htmlFor="no">no</label>
+        </div>
 
-          <br />
-                  
-        </fieldset>
-      </div>
 
-      <button  type="submit" className="btnQuiz"> Submit
-      </button>
-
-    </div>
-   
-    
-    
-  )
+    )
 }
 
 export default Quiz
